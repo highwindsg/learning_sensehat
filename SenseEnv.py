232b0d67a478df7e1f0bb273c85f747a9e77d19b
Caven Yip <highwind.sg@gmail.com>
2018-08-22 16:12:57 +0800

    new file
    new file

---
+++ SenseEnv.py
@@ -0,0 +1,21 @@
+from sense_hat import SenseHat
+import time
+from time import asctime
+
+sense = SenseHat()
+
+while True:
+    #temp = round(sense.get_temperature()*1.8 +32)  # Round off temp to Fahrenheit as a integer.
+    temp = int(sense.get_temperature())     # Obtain temperature reading as integer
+    humidity = int(sense.get_humidity())    # Obtain humidity reading as integer
+    pressure = int(sense.get_pressure())    # Obtain pressure reading as integer
+    #message = " T = %ddeg, H = %d, P = %d " %(temp, humidity, pressure)
+    message = " Temp: %s C, Humidity: %s rH, Pressure: %s Millibars " %(temp, humidity, pressure)
+    #sense.show_message(message, scroll_speed = (0.08), text_colour = [200,240,200], back_colour = [0,0,0])
+    time.sleep(4)
+    log = open('weather.txt', "a")  # Opens a weather text file and append
+    now = str(asctime())            # the current time and
+    log.write(now+''+message+'\n')  # write in with the message of the T, H and P.
+    print(message)  # At the same time, print out the message on screen.
+    log.close()     # Then close the log file for this run cycle.
+    time.sleep(5)   # Wait for 5 secs then restart the while loop again.
--- sensehat_txt_bkgd_color_loop.py
+++ sensehat_txt_bkgd_color_loop.py
@@ -9,7 +9,9 @@ yellow = (255, 255, 0)
 # back_colour alters the colour of the background
 # and works in the same way as text_colour.
 # The while loop ensures the message shows continuously
-# with a scrolling speed of 0.05.
+# with a scrolling speed of 0.08.
+# The default value is 0.1.
+# The bigger the number, the slower the speed.
 while True:
     sense.show_message("Sense Hat is awesome!", text_colour=yellow, back_colour=blue, scroll_speed=0.08)
     sleep(3)
