9a1cb9b149add1336bed0afca0dc70ddd4a4ff0e
highwind.sg <highwind.sg@gmail.com>
2019-07-11 07:11:32 +0800

    new files
    new files

---
+++ sensehat_set_pixel3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+sense.set_pixel(2, 2, (0, 0, 255))
+sense.set_pixel(4, 2, (0, 0, 255))
+sense.set_pixel(3, 4, (100, 0, 0))
+sense.set_pixel(1, 5, (255, 0, 0))
+sense.set_pixel(2, 6, (255, 0, 0))
+sense.set_pixel(3, 6, (255, 0, 0))
+sense.set_pixel(4, 6, (255, 0, 0))
+sense.set_pixel(5, 5, (255, 0, 0))
+
+while True:
+    sense.set_rotation(180)
+    sleep(2)
+    sense.flip_h()
+    sleep(2)
+    sense.flip_v()
+    #sleep(2)
+    #sense.clear()
+
+
