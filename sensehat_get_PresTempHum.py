435c2fb754ba17ad409527b956e60fb1210c6942
highwind.sg <highwind.sg@gmail.com>
2019-07-12 06:59:54 +0800

    new files
    new files

---
+++ sensehat_get_PresTempHum.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+sense.clear()
+
+pressure = sense.get_pressure()
+pressure = round(pressure, 1)
+print("Pressure now is", pressure)
+
+temp = sense.get_temperature()
+temp = round(temp, 1)
+print("Temperature now is", temp)
+
+humidity = sense.get_humidity()
+humidity = round(humidity, 1)
+print("Humidity now is", humidity)
+
+while True:
+    message = "Temperature: " + str(temp) + " Pressure: " + str(pressure) + " Humidity: " + str(humidity)
+    sense.show_message(message, scroll_speed = 0.1)
